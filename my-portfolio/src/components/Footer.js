d8e25672246c92b90358a6d2cbb5529b36c87e21
kylechau123 <123038673+kylechau123@users.noreply.github.com>
2023-06-28 17:01:00 -0400

    formatted basic html elements in the about and contact pages using bulma framework, created footer for page

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 function Footer(props) {
     return (
-        <div>
-            
-        </div>
+        <footer class="footer">
+            <div class="content has-text-centered">
+            <a href = "https://github.com/kylechau123"><i class="fa-brands fa-github m-5 is-size-2"></i></a>
+            <a href= "https://www.linkedin.com/in/kyle-chau-a8727926a/"><i class="fa-brands fa-linkedin m-5 is-size-2"></i></a>
+            <a href= "https://www.instagram.com/kylechaaau/"><i class="fa-brands fa-instagram m-5 is-size-2"></i></a>
+            </div>
+        </footer>
     );
 }
 
